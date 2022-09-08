a2d12a7b002abe8c196024ac368fce835b1ef19a
Cazolion <redhead.terror@gmail.com>
2022-09-08 14:47:20 +1000

    Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let wordCheck = word.length;
+    if (num < 0 || wordCheck < 0) {
+        return "ERROR";
+    }
+     else {
+        for (let i = 0; i <= num; i++) {
+        return word.repeat(num);
+        }
+    }
 };
 
 // Do not edit below this line
