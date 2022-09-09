78bad94d5161d85f5935470aa966428cfcc8d20a
Cazolion <redhead.terror@gmail.com>
2022-09-09 15:05:58 +1000

    Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+        let sliceText = "";
+        let reverseWord = "";
+        
+        for (let i=0; i <= word.length; i++) {
+            sliceText = word.slice(i,i+1);
+            reverseWord = sliceText + reverseWord;
+        }
+        return reverseWord;
+    
 };
 
 // Do not edit below this line
