4aa1bd8e63c2aefc77a48d2bd41892b916e19f31
Cazolion <redhead.terror@gmail.com>
2022-09-09 23:04:19 +1000

    Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    
+    let test = 0;
+    if (Math.sign(first) === -1 || Math.sign(second) === -1 || typeof(first) !== "number" || typeof(second) !== "number") {
+        return "ERROR";
+    }
+    else if (first > second) {
+    for(let i = second; i <= first; i++) {
+        test += i;
+    }
+    }
+    else {
+    for(let i = first; i <= second; i++) {
+    test += i;
+    }
+}
+
+return test;
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
