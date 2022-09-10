141a8d58c8af86894455f576093e5eb70235d71a
Cazolion <redhead.terror@gmail.com>
2022-09-10 14:37:43 +1000

    Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(ftemp) {
+
+let celTemp = (ftemp - 32) * 5/9;
+let celTempRounded = Math.round(celTemp * 10) / 10;
+return celTempRounded;
 
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  
+  let farTemp = ctemp * 9/5 + 32;
+  let farTempRounded = Math.round(farTemp * 10) / 10;
+  return farTempRounded;
 };
 
 // Do not edit below this line
