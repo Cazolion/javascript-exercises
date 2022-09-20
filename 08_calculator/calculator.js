fd3149349c2a2471f4e4d76f255cbdf3f3403bb0
Cazolion <redhead.terror@gmail.com>
2022-09-20 21:12:42 +1000

    Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentValue) => {
+    return total + currentValue
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentValue) => {
+    return total * currentValue
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  else {
+    let fact = 1;
+	  for (i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact
+}
 };
 
 // Do not edit below this line
